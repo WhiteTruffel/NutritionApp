33028ea0abdb6ece79cd3ac558f08002919bbb6b
tobiaskochberlin-svg <tobiaskochberlin@me.com>
2026-05-30 12:44:30 +0200

    Initial Commit

---
+++ NutritionApp/NutritionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutritionAppApp.swift
+//  NutritionApp
+//
+//  Created by Tobias Koch on 30.05.26.
+//
+
+import SwiftUI
+
+@main
+struct NutritionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
